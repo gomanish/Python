41bcf982e67410def9161cac9d6aade534d1a576
Manish Kumar <46210835+gomanish@users.noreply.github.com>
2019-12-24 10:32:19 +0530

    Update 2nd.py

--- 2nd.py
+++ 2nd.py
@@ -1,4 +1,5 @@
-# Write a program that print the integers from 1 to 100. But the multiple of three print "Fizz" instead of the number, and for the multiple of five print "Buzz" .for numbers which are multiple of both three and five print "FizzBuzz".
+'''Write a program that print the integers from 1 to 100. But the multiple of three print "Fizz" instead of the number,
+and for the multiple of five print "Buzz" .for numbers which are multiple of both three and five print "FizzBuzz".'''
 
 for num in range(1,101):
 	if num%3==0 and num%5==0:
