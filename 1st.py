308047ee8ff2c354757f23e3d5b6dc1110f6e587
Manish Kumar <46210835+gomanish@users.noreply.github.com>
2019-12-25 12:56:44 +0530

    Delete 1st.py

--- 1st.py
+++
@@ -1,6 +0,0 @@
-# Use for, .split() and if to create a statement that will print out words that start with 's':
-
-st='Sam print only the words that start with s in this sentence'
-world=[x for x in st.split() if x[0].lower()=='s' ]
-for z in world:
-	print(z)
