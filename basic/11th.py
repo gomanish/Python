ffa6f21856a74663871cc0dd53ec7f0f2326f2f9
Manish Kumar <46210835+gomanish@users.noreply.github.com>
2019-12-27 20:05:10 +0530

    ALMOST THERE

---
+++ basic/11th.py
@@ -0,0 +1,9 @@
+''' ALMOST THERE: Given an integer n, return True if n is within 10 of either 100 or 200'''
+
+def almost_there(n):
+		return(abs(n-100)<=10 or abs(n-200)<=10)
+
+print(almost_there(90))
+print(almost_there(104))
+print(almost_there(150))
+print(almost_there(209))
