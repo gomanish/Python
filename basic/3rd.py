008fd55480a64f90a4f7df484671088650f67609
Manish Kumar <46210835+gomanish@users.noreply.github.com>
2020-01-11 21:48:00 +0530

    list of the first letters
    create a list of the first letters of every word in the string below

---
+++ basic/3rd.py
@@ -0,0 +1,6 @@
+# create a list of the first letters of every word in the string below
+
+
+st='Create a list of the first letters of every word in the string'
+List=[x[0] for x in st.split()]
+print(List)
